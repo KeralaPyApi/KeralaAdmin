f055246ed6535088ba1ec529c03ecbbaf097376b
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-01-27 19:16:16 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+#####################################
+import os
+
+token = ''
+
+admin_id = 12345678
+admin_name = ''
+
+#####################################
+
+import keralabot
+
+from keralabot import types
+
+bot = keralabot.Bot(token)
+
