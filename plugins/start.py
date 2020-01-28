4909b72af8ed5fceb19d6e4bf0b71ebbb7480d18
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-01-28 16:06:36 +0530

    Updated start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,7 +7,7 @@ Keralabot.logger.setLevel(logging.DEBUG) # Outputs debug messages to console.
 from config import *
 
 @bot.message_handler(commands=['start'])
-def start (message)
+def start(message):
     if message.chat.type == "private":
         bot.reply_to(message, "<b>Hi I am an Admin bot written using KeralaPyApi.</b>", parse_mode="HTML")
     else:
