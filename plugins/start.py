e6e6fe65c9743c9736862c23071f138ce432b31b
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-01-28 21:26:37 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,9 +7,9 @@ keralabot.logger.setLevel(logging.DEBUG) # Outputs debug messages to console.
 from config import *
 
 @bot.message_handler(commands=['start'])
-def start(message, args[]):
-    if len(args) >= 1:
-            if args[0].lower() == "help":
+def start(message):
+    if len(message) >= 1:
+            if message[0].lower() == "help":
                 bot.reply_to(message, "Help")
     if message.chat.type == "private":
         bot.send_chat_action(message.chat.id, "typing")
