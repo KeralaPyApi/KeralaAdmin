d5488f4921eaffdffd6f7a01fcc3b0dab64e8d59
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-01-29 19:45:22 +0530

    Typo error

--- plugins/ping.py
+++ plugins/ping.py
@@ -5,7 +5,7 @@ from config import *
 def ping(message):
     if message.text == "/ping" or message.text == "!ping":
         first = datetime.now()
-        ping = bot.send_message(message.chat.id, **Ping!**, parse_mode='Markdown')
+        ping = bot.send_message(message.chat.id, "**Ping!**", parse_mode='Markdown')
         second = datetime.now()
         bot.edit_message_text(message.chat.id, ping.chat.id, "**Pong!**\n`{}`".format((second - first).microseconds / 1000), parse_mode='Markdown')
         return true
