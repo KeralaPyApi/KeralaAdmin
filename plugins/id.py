8a687e0ade8cc75f7a16ec99e5add66d4ee08a6f
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-01-30 20:15:15 +0530

    Update id.py

--- plugins/id.py
+++ plugins/id.py
@@ -8,6 +8,5 @@ from config import *
 
 @bot.message_handler(commands=['id'])
 def getids(message):
-    if message.chat.type == "private":
-        bot.reply_to(message, "*Information of the user*\n\n*Name :* `{}`\n*Username :* `{}`\n*User ID :* `{}`".format(message.from.first_name, message.from.username, message.from.id), parse_mode='Markdown')
-        return
+    bot.send_message(message, "*Information of the user*\n\n*Name :* `{}`\n*Username :* `{}`\n*User ID :* `{}`".format(message.from.first_name, message.from.username, message.from.id), parse_mode='Markdown')
+    return
