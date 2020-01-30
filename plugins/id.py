6799b506b620e847ea0c4c17b6a75acc668e6401
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-01-30 20:06:11 +0530

    Typo

--- plugins/id.py
+++ plugins/id.py
@@ -4,9 +4,5 @@ from config import *
 @bot.message_handler(commands=['id'])
 def get_id(message):
     if message.chat.type == "private":
-        bot.reply_to(message.chat.id, '''*Information of the user*
-
-*Name :* `{}`
-*Username :* `{}`
-*User ID :* `{}`'''.format(message.from.first_name, message.from.username, message.from.id), parse_mode='Markdown')
+        bot.reply_to(message, "*Information of the user*\n\n*Name :* `{}`\n*Username :* `{}`\n*User ID :* `{}`".format(message.from.first_name, message.from.username, message.from.id), parse_mode='Markdown')
         return
