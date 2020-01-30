67c548d83158ce46a01294b256779f9c84479733
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-01-30 18:54:37 +0530

    Stabled  ping and legal markdown added

--- plugins/ping.py
+++ plugins/ping.py
@@ -8,5 +8,5 @@ def ping(message):
         first = datetime.now()
         ping = bot.send_message(message.chat.id, "**Ping!**", parse_mode='Markdown')
         second = datetime.now()
-        bot.edit_message_text("**Pong!**\n`{}`".format((second - first).microseconds / 1000), message.chat.id, ping.message_id, parse_mode='Markdown')
-        return true
+        bot.edit_message_text("*Pong!*\n`{}`".format((second - first).microseconds / 1000), message.chat.id, ping.message_id, parse_mode='Markdown')
+        return 
