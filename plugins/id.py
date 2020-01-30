c567304e8c5fe5041497b6b2ae2181af566565a4
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-01-30 20:11:22 +0530

    Update id.py

--- plugins/id.py
+++ plugins/id.py
@@ -1,8 +1,13 @@
 import keralabot
+import logging
+
+logger = keralabot.logger
+keralabot.logger.setLevel(logging.DEBUG) # Outputs debug messages to console.
+
 from config import *
 
 @bot.message_handler(commands=['id'])
-def get_id(message):
+def getids(message):
     if message.chat.type == "private":
         bot.reply_to(message, "*Information of the user*\n\n*Name :* `{}`\n*Username :* `{}`\n*User ID :* `{}`".format(message.from.first_name, message.from.username, message.from.id), parse_mode='Markdown')
         return
