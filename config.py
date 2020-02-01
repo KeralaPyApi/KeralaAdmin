650deb89d2913b85167faa907a2fc9cd03226446
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-02-01 06:23:18 +0530

    Added botname for group functions

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ token = os.environ.get("TOKEN", "")
 
 admin_id = int(os.environ.get("ADMIN_ID", ""))
 admin_name = os.environ.get("ADMIN_USERNAME", "")
+botname = os.environ.get("BOT_NAME", "") # USERNAME of the bot without '@'
 
 #####################################
 
