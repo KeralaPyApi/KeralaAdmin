c7e595f8330a42f3249c5d1bfce26bd362d107d9
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-02-01 15:40:37 +0530

    Update id.py

--- plugins/id.py
+++ plugins/id.py
@@ -12,6 +12,6 @@ def tg_id(message):
         bot.reply_to(message, "*Information of user*\n\n*Name* : `{}`\n*ID *: `{}`".format(message.from_user.first_name, message.from_user.id), parse_mode="Markdown")
     if message.chat.type != "private" and message.reply_to_message == None:
         bot.reply_to(message, "*Information of the user*\n\n*Name* : `{}`\n*ID* : `{}`\n\n*Information of the chat*\n\n*Name of chat* : `{}`\n*ID of chat* : `{}`\n*Type of Chat* : `{}`".format(message.from_user.first_name, message.from_user.id, message.chat.title, message.chat.id, message.chat.type), parse_mode="Markdown")
-    if message.chat.type != 'private" and message.reply_to_message != None:
+    if message.chat.type != "private" and message.reply_to_message != None:
         bot.reply_to(message, "*Information of the user*\n\n*Name* : `{}`\n*ID* : `{}`\n\n*Information of the chat*\n\n*Name of chat* : `{}`\n*ID of chat* : `{}`\n*Type of Chat* : `{}`".format(message.reply_to_message.from_user.first_name, message.reply_to_message.from_user.id, message.chat.title, message.chat.id, message.chat.type), parse_mode="Markdown")
 
