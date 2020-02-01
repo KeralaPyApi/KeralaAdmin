7ba0c29d4c67a7f3a70e69b9d0677db742d62cca
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-02-01 08:29:46 +0530

    Create id.py

---
+++ plugins/id.py
@@ -0,0 +1,13 @@
+import keralabot
+from keralabot.types import InlineKeyboardMarkup, InlineKeyboardButton
+import logging
+
+logger = keralabot.logger
+keralabot.logger.setLevel(logging.DEBUG) # Outputs debug messages to console.
+
+from config import *
+
+@bot.message_handler(commands=['id'])
+def tg_id(message):
+    if message.chat.type == "private":
+        bot.reply_to(message, "*Information of user*\n\n*Name* : `{}`\n*ID *: `{}`".format(message.from.first_name, message.from.id)) 
