6587e80e2e5e699d46283b6b0e6f980c969830ae
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-02-01 15:38:39 +0530

    Added new updation

--- plugins/id.py
+++ plugins/id.py
@@ -1,5 +1,4 @@
 import keralabot
-from keralabot.types import InlineKeyboardMarkup, InlineKeyboardButton
 import logging
 
 logger = keralabot.logger
@@ -10,4 +9,9 @@ from config import *
 @bot.message_handler(commands=['id'])
 def tg_id(message):
     if message.chat.type == "private":
-        bot.reply_to(message, "*Information of user*\n\n*Name* : `{}`\n*ID *: `{}`".format(message.from_user.first_name, message.from_user.id), parse_mode="Markdown") 
+        bot.reply_to(message, "*Information of user*\n\n*Name* : `{}`\n*ID *: `{}`".format(message.from_user.first_name, message.from_user.id), parse_mode="Markdown")
+    if message.chat.type != "private" and message.reply_to_message == None:
+        bot.reply_to(message, "*Information of the user*\n\n*Name* : `{}`\n*ID* : `{}`\n\n*Information of the chat*\n\n*Name of chat* : `{}`\n*ID of chat* : `{}`\n*Type of Chat* : `{}`".format(message.from_user.first_name, message.from_user.id, message.chat.title, message.chat.id, message.chat.type), parse_mode="Markdown")
+    if message.chat.type != 'private" and message.reply_to_message != None:
+        bot.reply_to(message, "*Information of the user*\n\n*Name* : `{}`\n*ID* : `{}`\n\n*Information of the chat*\n\n*Name of chat* : `{}`\n*ID of chat* : `{}`\n*Type of Chat* : `{}`".format(message.reply_to_message.from_user.first_name, message.reply_to_message.from_user.id, message.chat.title, message.chat.id, message.chat.type), parse_mode="Markdown")
+
