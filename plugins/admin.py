8f3bab70aa00ce17ba381ab580b38e172a1fe333
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-02-02 13:53:05 +0530

    Typo fixation

--- plugins/admin.py
+++ plugins/admin.py
@@ -9,9 +9,7 @@ def ban(message):
     chat_id = message.chat.id
     user_id = message.from_user.id
     members = bot.get_chat_member(chat_id, user_id)
-    if members.status != "administrator" or members.status != "creator":
-        bot.reply_to(message, "Who are you Non - Admin to command me")
-        return
+    
     if message.chat.type == "private":
         bot.reply_to(message, "This command is meant to be used in Groups")
         return
@@ -26,6 +24,9 @@ def ban(message):
         bot.kick_chat_member(message.chat.id, message.reply_to_message.from_user.id)
         bot.reply_to(message, "{} banned {}".format(message.from_user.first_name, message.reply_to_message.from_user.first_name))
         return
+    if members.status != "administrator" or members.status != "creator":
+        bot.reply_to(message, "Who are you Non - Admin to command me")
+        return
 
 @bot.message_handler(commands=['unban'])
 def unban(message):
@@ -35,9 +36,7 @@ def unban(message):
     if message.chat.type == "private":
         bot.reply_to(message, "This command is meant to be used in Groups")
         return
-    if members.status != "administrator" or members.status != "creator":
-        bot.reply_to(message, "Who are you Non - Admin to command me")
-        return
+    
     if message.reply_to_message == None and members.status == "administrator" or members.status == "creator":
         unban_user = message.text[7:]
         if unban_user == None:
@@ -48,3 +47,7 @@ def unban(message):
     if message.reply_to_message != None and members.status == "administrator" or members.status == "creator":
         bot.unban_chat_member(message.chat.id, message.reply_to_message.from_user.id)
         bot.reply_to(message, "Now that user can join this chat")
+        return
+    if members.status != "administrator" or members.status != "creator":
+        bot.reply_to(message, "Who are you Non - Admin to command me")
+        return
