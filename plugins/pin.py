5dfa2072bad1a21b3c7ca64621b78acea440fea4
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-02-02 08:14:08 +0530

    Some random improvements

--- plugins/pin.py
+++ plugins/pin.py
@@ -18,5 +18,7 @@ def pin(message):
         bot.reply_to(message, "This command is meant to be used in Groups")
     if message.reply_to_message == None:
         bot.reply_to(message, "Reply to a message to be pinned")
-    if members.status == "administrator":
+    if members.status == "administrator" or members.status == "creator":
         bot.pin_chat_message(message.chat.id, message.reply_to_message.message_id)
+    if members.status != "administrator":
+        bot.reply_to(message, "You need to be an Admin to pin the message")
