e29c5aa1943c62cd2cff17090318aaa96b0bbc96
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-02-02 13:47:23 +0530

    Typo error debugged

--- plugins/admin.py
+++ plugins/admin.py
@@ -9,8 +9,12 @@ def ban(message):
     chat_id = message.chat.id
     user_id = message.from_user.id
     members = bot.get_chat_member(chat_id, user_id)
+    if members.status != "administrator" or members.status != "creator":
+        bot.reply_to(message, "Who are you Non - Admin to command me")
+        return
     if message.chat.type == "private":
         bot.reply_to(message, "This command is meant to be used in Groups")
+        return
     if message.reply_to_message == None and members.status == "administrator" or members.status == "creator":
         ban_user = message.text[5:]
         if ban_user == None:
@@ -18,13 +22,10 @@ def ban(message):
         else:
             bot.kick_chat_member(message.chat.id, ban_user)
             bot.reply_to(message, "{} banned {}".format(message.from_user.first_name, members.first_name))
-    if members.status != "administrator" or members.status != "creator":
-        bot.reply_to(message, "Who are you Non - Admin to command me")
     if message.reply_to_message != None and members.status == "administrator" or members.status == "creator":
         bot.kick_chat_member(message.chat.id, message.reply_to_message.from_user.id)
         bot.reply_to(message, "{} banned {}".format(message.from_user.first_name, message.reply_to_message.from_user.first_name))
-    if members.status != "administrator" or members.status != "creator":
-        bot.reply_to(message, "Who are you Non - Admin to command me")
+        return
 
 @bot.message_handler(commands=['unban'])
 def unban(message):
@@ -33,6 +34,10 @@ def unban(message):
     members = bot.get_chat_member(chat_id, user_id)
     if message.chat.type == "private":
         bot.reply_to(message, "This command is meant to be used in Groups")
+        return
+    if members.status != "administrator" or members.status != "creator":
+        bot.reply_to(message, "Who are you Non - Admin to command me")
+        return
     if message.reply_to_message == None and members.status == "administrator" or members.status == "creator":
         unban_user = message.text[7:]
         if unban_user == None:
@@ -40,11 +45,6 @@ def unban(message):
         else:
             bot.unban_chat_member(message.chat.id, unban_user)
             bot.reply_to(message, "Now that user can join this chat")
-    if members.status != "administrator" or members.status != "creator":
-        bot.reply_to(message, "Who are you Non - Admin to command me")
     if message.reply_to_message != None and members.status == "administrator" or members.status == "creator":
         bot.unban_chat_member(message.chat.id, message.reply_to_message.from_user.id)
         bot.reply_to(message, "Now that user can join this chat")
-    if members.status != "administrator" or members.status != "creator":
-        bot.reply_to(message, "Who are you Non - Admin to command me")
-
