b873d577356207c87447c0b0f597e8a961b6f141
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-02-05 21:31:03 +0530

    Beta welcome 1.0

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -5,8 +5,8 @@ import logging
 
 from config import *
 
-@bot.message_handler(types.Message.new_chat_member)
+@bot.message_handler(types.new_chat_member)
 def welcome(message):
-    new_members = types.Message.new_chat_member
+    new_members = types.new_chat_member
     for nm in new_members:
         bot.reply_to(message, "Hello {first} welcome.".format(nm.first_name))
