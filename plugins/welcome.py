4c631eab2d9f1598e3444ac60acfe100c476f101
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-02-05 21:52:21 +0530

    Welcome 1.0

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -5,7 +5,7 @@ import logging
 
 from config import *
 
-@bot.message_handler(func=lambda message: True)
+@bot.message_handler(content_types=['new_chat_members'])
 def welcome(message):
     if message.new_chat_member != None:
         bot.reply_to(message, "Hello {first} welcome.".format(message.new_chat_member.first_name))
