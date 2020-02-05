e6c500859865ff15e364d1d6fc5af0231ec54bba
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-02-05 20:48:45 +0530

    Some returning error debugged

--- plugins/pin.py
+++ plugins/pin.py
@@ -16,10 +16,14 @@ def pin(message):
     members = bot.get_chat_member(chat_id, user_id)
     if message.chat.type == "private":
         bot.reply_to(message, "This command is meant to be used in Groups")
+        return
     if message.reply_to_message == None:
         bot.reply_to(message, "Reply to a message to be pinned")
+        return
     if members.status == "administrator" or members.status == "creator":
         bot.pin_chat_message(message.chat.id, message.reply_to_message.message_id)
         bot.delete_message(message.chat.id, message.message_id)      #Added delete some command for misuse of that
+        return
     if members.status != "administrator":
         bot.reply_to(message, "You need to be an Admin to pin the message")
+        return
