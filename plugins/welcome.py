8eeccfe0845c48a6fb014bd9e20a6806b5307f3d
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-02-05 21:58:45 +0530

    Some typo fix for welcome 1.0

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -8,4 +8,4 @@ from config import *
 @bot.message_handler(content_types=['new_chat_members'])
 def welcome(message):
     if message.new_chat_member != None:
-        bot.reply_to(message, "Hello {} welcome.".format(message.new_chat_members.first_name))
+        bot.reply_to(message, "Hello {} welcome.".format(message.from_user.first_name))
