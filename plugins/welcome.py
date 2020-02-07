e4c2daa76b1828ce5cabb76f36167ffa671b3a71
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-02-07 21:39:30 +0530

    Some new fixes

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -4,8 +4,41 @@ from keralabot import types
 import logging
 
 from config import *
+from database import *
+
+def markdown(text):
+    text = text.replace(']', '\]')
+    text = text.replace('[', '\[')
+    text = text.replace('_', '\_')
+    text = text.replace('*', '\*')
+    text = text.replace('`', '\`')
+
+    return text
+
+def get_welcome(chat_id):
+    cursor.execute('SELECT welcome, welcome_enabled FROM chats WHERE chat_id = (?)', (chat_id,))
+    try:
+        return cursor.fetchone()
+    except IndexError:
+        return None
+
+
+def set_welcome(chat_id, welcome):
+    cursor.execute('UPDATE chats SET welcome = ? WHERE chat_id = ?', (welcome, chat_id))
+    conn.commit()
+
 
 @bot.message_handler(content_types=['new_chat_members'])
 def welcome(message):
-    if message.new_chat_member != None:
-        bot.reply_to(message, "Hello {} welcome.".format(message.from_user.first_name))
+    chat_id = message.chat.id
+    chat_title = message.chat.title
+    first_name = message.new_chat_member.first_name
+    welcome = get_welcome(chat_id)
+    if welcome[1]:
+        if welcome[0] is not None:
+            welcome = welcome[0].replace('{id}', str(chat_id))
+            welcome = welcome.replace('{title}', escape_markdown(chat_title))
+            welcome = welcome.replace('{name}', escape_markdown(first_name))
+        else:
+            welcome = "Hello {} welcome.".format(message.from_user.first_name)
+        bot.reply_to(message, welcome)
