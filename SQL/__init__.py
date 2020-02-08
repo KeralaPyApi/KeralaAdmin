c9f0b68e6ff5d6466774abd2b2e92a28d4f27ab4
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-02-08 19:03:42 +0530

    Use sqlalchemy instead of direct sql

---
+++ SQL/__init__.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+DB = 'bot.db'
+
+def start() -> scoped_session:
+    engine = create_engine(DB, client_encoding="utf8")
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+BASE = declarative_base()
+SESSION = start()
