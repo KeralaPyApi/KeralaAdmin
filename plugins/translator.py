82caa5a602d06721a81aa10e1c9be0c57fdb97c9
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-02-09 17:01:32 +0530

    Translator added

---
+++ plugins/translator.py
@@ -0,0 +1,18 @@
+import keralabot
+import logging
+from googletrans import Translator
+
+@bot.message_handler(commands=['tr'])
+def translate(message):
+    to_translate_text = message.reply_to_message.text
+    lang = message.text[4:]
+    if lang == None:
+        lang = "en"
+    translator = Translator()
+    try:
+        translated = translator.translate(to_translate_text, dest=lang)
+        src_lang = translated.src
+        translated_text = translated.text
+        bot.reply_to(message, "Translated from {} to {}.\n\n{}.".format(src_lang, lang, translated_text))
+    except:
+        bot.reply_to(message, "Error in Translation")
