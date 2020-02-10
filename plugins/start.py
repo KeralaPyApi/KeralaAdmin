4297a76251d85880f8a6bd08075d81a3b1d5ba2a
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-02-10 19:23:13 +0530

    Modules help added

--- plugins/start.py
+++ plugins/start.py
@@ -14,6 +14,13 @@ def markup():
                                InlineKeyboardButton("Add me to group", url="t.me/{}?startgroup=new".format(botname)))
     return markup
 
+def help_markup():
+    help_mark = InlineKeyboardMarkup()
+    help_mark.row_width = 2
+    help_mark.add(InlineKeyboardButton("Admin", callback_data="admin"), InlineKeyboardButton("Greetings", callback_data="welcome"))
+    help_mark.add(InlineKeyboardButton("Google Translate", callback_data="translate"), InlineKeyboardButton("Misc", callback_data="misc"))
+    return help_mark
+
 @bot.message_handler(commands=['start'])
 def start(message):
     if message.chat.type == "private":
@@ -27,4 +34,13 @@ def start(message):
 def callback_query(call):
     if call.data == "help":
         bot.answer_callback_query(call.id, "On beta mode now")
+        bot.edit_message_text("There are many things that I can do. But now I am in Beta 🚼 mode.\n\nThese are some of my modules.", call.message.chat.id, call.message.message_id, reply_markup=help_mark())
+    if call.data == "admin":
         bot.edit_message_text("On beta mode now", call.message.chat.id, call.message.message_id)
+    if call.data == "welcome":
+        bot.edit_message_text("On beta mode now", call.message.chat.id, call.message.message_id)
+    if call.data == "translate":
+        bot.edit_message_text("On beta mode now", call.message.chat.id, call.message.message_id)
+    if call.data == "misc":
+        bot.edit_message_text("On beta mode now", call.message.chat.id, call.message.message_id)
+
