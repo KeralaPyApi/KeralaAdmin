dfdbceeacd52095c228c6c2e52270586014a0cf0
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-02-16 14:08:46 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -29,7 +29,7 @@ def help_back():
     return help_back
 
 @bot.message_handler(commands=['start'])
-def start(message, List[str]):
+def start(message, args):
     args = List[str]
     if message.chat.type == "private":
         if len(args) >= 1:
