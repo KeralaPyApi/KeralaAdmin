47f585c666aeee80a35a520f8b00c9c3e66f9a23
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-02-17 14:14:41 +0530

    Update funcs.py

--- plugins/funcs.py
+++ plugins/funcs.py
@@ -579,7 +579,7 @@ def get_welcome_type(msg):
     # determine what the contents of the filter are - text, image, sticker, etc
     if len(args) >= 2:
         offset = len(args[1]) - len(msg.text)  # set correct offset relative to command + notename
-        text, buttons = button_markdown_parser(args[1], entities=msg.parse_entities(), offset=offset)
+        text, buttons = button_markdown_parser(args[1], entities=None, offset=offset)
         if buttons:
             data_type = Types.BUTTON_TEXT
         else:
