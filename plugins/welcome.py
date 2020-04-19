469823f59f0d9fba338478f9546c1ccfad4036e2
Kerala Developer Team[ ★彡 Anand 彡★] <62738084+Anandpskerala@users.noreply.github.com>
2020-04-19 20:40:23 +0530

    Welcome status updated

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -8,64 +8,9 @@ from database import *
 import SQL.welcome_sql as sql
 from plugins.funcs import get_welcome_type, build_keyboard
 from keralabot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from keralabot.button.base import escape_markdown
 
 #VALID_WELCOME_FORMATTERS = ['first']
-
-def escap_markdown(text):
-    text = text.replace('[', '\[')
-    text = text.replace('_', '\_')
-    text = text.replace('*', '\*')
-    text = text.replace('`', '\`')
-
-    return text
-
-def escape_invalid_curly_brackets(text, valids):
-    new_text = ""
-    idx = 0
-    while idx < len(text):
-        if text[idx] == "{":
-            if idx + 1 < len(text) and text[idx + 1] == "{":
-                idx += 2
-                new_text += "{{{{"
-                continue
-            else:
-                success = False
-                for v in valids:
-                    if text[idx:].startswith('{' + v + '}'):
-                        success = True
-                        break
-                if success:
-                    new_text += text[idx: idx + len(v) + 2]
-                    idx += len(v) + 2
-                    continue
-                else:
-                    new_text += "{{"
-
-        elif text[idx] == "}":
-            if idx + 1 < len(text) and text[idx + 1] == "}":
-                idx += 2
-                new_text += "}}}}"
-                continue
-            else:
-                new_text += "}}"
-
-        else:
-            new_text += text[idx]
-        idx += 1
-
-    return new_text
-
-#def get_welcome(chat_id):
-#    cursor.execute('SELECT welcome FROM chats WHERE chat_id = (?)', (chat_id,))
-#    try:
-#        return cursor.fetchone()
-#    except IndexError:
-#        return None
-#
-#
-#def set_welcome(chat_id, welcome):
-#    cursor.execute('UPDATE chats SET welcome = ? WHERE chat_id = ?', (welcome, chat_id))
-#    conn.commit()
 
 
 @bot.message_handler(content_types=['new_chat_members'])
@@ -77,12 +22,10 @@ def welcome(message):
     mention = "[{}](tg://user?id={})".format(message.new_chat_member.first_name, message.from_user.id)
     members = bot.get_chat_members_count(chat_id)
     cust_welcome = sql.get_welc_pref(chat_id)
-    #valid_format = escape_invalid_curly_brackets(cust_welcome, VALID_WELCOME_FORMATTERS)
-    #res = valid_format.format(first=escap_markdown(first_name)) 
-    cust_welcome = cust_welcome.replace('{name}', escap_markdown(first_name))
-    cust_welcome = cust_welcome.replace('{fullname}', escap_markdown(full_name))
+    cust_welcome = cust_welcome.replace('{name}', escape_markdown(first_name))
+    cust_welcome = cust_welcome.replace('{fullname}', escape_markdown(full_name))
     cust_welcome = cust_welcome.replace('{mention}', (mention))
-    cust_welcome = cust_welcome.replace('{title}', escap_markdown(chat_title))
+    cust_welcome = cust_welcome.replace('{title}', escape_markdown(chat_title))
     buttons = sql.get_welc_buttons(chat_id)
     keyb = build_keyboard(buttons)
     keyboard = InlineKeyboardMarkup(keyb)
