30ca0f06fdd2e16f4dbf3803213e0be3099100ea
[ ★彡 Anand 彡★] <62738084+Anandpskerala@users.noreply.github.com>
2020-04-27 16:13:57 +0530

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -1,3 +1,13 @@
 import BotClient
 
+from datetime import datetime
+
+cf = datetime.now()
 BotClient.new("Token", "send_message")
+cs = datetime.now()
+print(f"{(cs - cf).microseconds / 1000} ms")
+
+pf = datetime.now()
+print("successful login Token\nMethod send")
+ps = datetime.now()
+print(f"{(ps - pf).microseconds / 1000} ms")
